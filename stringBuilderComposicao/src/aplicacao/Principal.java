9959ce7edfe915765701d1c03e2b693d68f6c29b
Rafael Silva <rsilvalauriano@gmail.com>
2023-09-13 16:48:58 -0300

    commit 5

--- stringBuilderComposicao/src/aplicacao/Principal.java
+++ stringBuilderComposicao/src/aplicacao/Principal.java
@@ -1,13 +1,24 @@
 package aplicacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import entities.Comment;
+import entities.Post;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
 		Comment c1 = new Comment("Faça uma boa viajem!");
 		Comment c2 = new Comment("uau, isso é incrivel!!");
+		
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:11"), "Assis", "Uma cidade bonita", 500);
+		
+		p1.addComment(c1);
+		p1.addComment(c2);
 		
 		
 		
