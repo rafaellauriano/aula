330ed0683fcfa5fc80e4ca28e46363121fb6e0af
Rafael Silva <rsilvalauriano@gmail.com>
2023-09-13 16:15:23 -0300

    primeiro commit

---
+++ stringBuilderComposicao/src/aplicacao/Principal.java
@@ -0,0 +1,17 @@
+package aplicacao;
+
+import entities.Comment;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Comment c1 = new Comment("Have a nice trip!");
+		Comment c2 = new Comment("wow that's awesome!");
+		
+		
+		
+
+	}
+
+}
